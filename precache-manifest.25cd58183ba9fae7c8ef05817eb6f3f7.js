307aadadbbb0517f165a122660b2cd561c80517e
pidderliao <guanwei0702@gmail.com>
2020-09-10 18:49:35 +0800

    Updates

---
+++ precache-manifest.25cd58183ba9fae7c8ef05817eb6f3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbb56fb9a5f26c57d2b35b42bc302e5",
+    "url": "/eclothesSPA/index.html"
+  },
+  {
+    "revision": "3acee6f7b8238660681d",
+    "url": "/eclothesSPA/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "9d36e1ec763abe1c200a",
+    "url": "/eclothesSPA/static/css/main.e2448e37.chunk.css"
+  },
+  {
+    "revision": "3acee6f7b8238660681d",
+    "url": "/eclothesSPA/static/js/2.bcd8b52a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eclothesSPA/static/js/2.bcd8b52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d36e1ec763abe1c200a",
+    "url": "/eclothesSPA/static/js/main.7e6566c5.chunk.js"
+  },
+  {
+    "revision": "45660fd90d026b7e54e3",
+    "url": "/eclothesSPA/static/js/runtime-main.257c0a3c.js"
+  }
+]);
